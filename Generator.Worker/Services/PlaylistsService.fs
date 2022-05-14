0f381c7f69f016213d6228dce756d9fcf9499f26
Oleg <infinitu1327@gmail.com>
2022-05-14 17:31:20 +0300

    Update logging

--- Generator.Worker/Services/PlaylistsService.fs
+++ Generator.Worker/Services/PlaylistsService.fs
@@ -6,23 +6,25 @@ open Microsoft.EntityFrameworkCore
 open Shared.Data
 
 type PlaylistsService(_playlistService: PlaylistService, _logger: ILogger<PlaylistsService>, _context: AppDbContext) =
-    member _.ListTracksIdsAsync userId refreshCache =
-        task {
-            _logger.LogInformation("Listing playlists tracks ids")
+  member _.ListTracksIdsAsync userId refreshCache =
+    task {
+      let! playlistsUrls =
+        _context
+          .Playlists
+          .AsNoTracking()
+          .Where(fun p ->
+            p.UserId = userId
+            && p.PlaylistType = PlaylistType.Source)
+          .Select(fun p -> p.Url)
+          .ToListAsync()
 
-            let! playlistsUrls =
-                _context
-                    .Playlists
-                    .AsNoTracking()
-                    .Where(fun p ->
-                        p.UserId = userId
-                        && p.PlaylistType = PlaylistType.Source)
-                    .Select(fun p -> p.Url)
-                    .ToListAsync()
+      let! tracksIds = _playlistService.ListTracksIdsAsync userId playlistsUrls refreshCache
 
-            let! tracksIds = _playlistService.ListTracksIdsAsync userId playlistsUrls refreshCache
+      _logger.LogInformation(
+        "User with Telegram id {TelegramId} has {PlaylistsTracksIdsCount} tracks in source playlists",
+        userId,
+        tracksIds.Length
+      )
 
-            _logger.LogInformation("Playlists tracks count: {PlaylistsTracksIdsCount}", tracksIds.Length)
-
-            return tracksIds
-        }
+      return tracksIds
+    }
