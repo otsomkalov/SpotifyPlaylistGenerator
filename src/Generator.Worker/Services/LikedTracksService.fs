3fdeccede4fe99f2c710cbf28b8edf91dbf63f74
Oleg <otsomkalov@proton.me>
2023-03-03 17:40:06 +0200

    Do not load tracks from Spotify if they're already cached

--- src/Generator.Worker/Services/LikedTracksService.fs
+++ src/Generator.Worker/Services/LikedTracksService.fs
@@ -8,26 +8,27 @@ open Shared.Services
 
 type LikedTracksService(_spotifyClientProvider: SpotifyClientProvider, _idsService: TracksIdsService, _logger: ILogger<LikedTracksService>) =
   let rec downloadIdsAsync' (userId: int64) (offset: int) =
-    task {
-      let client =
-        _spotifyClientProvider.Get(userId)
+    fun () ->
+      task {
+        let client =
+          _spotifyClientProvider.Get(userId)
 
-      let! tracks = client.Library.GetTracks(LibraryTracksRequest(Offset = offset, Limit = 50))
+        let! tracks = client.Library.GetTracks(LibraryTracksRequest(Offset = offset, Limit = 50))
 
-      let! nextTracksIds =
-        if tracks.Next = null then
-          [] |> Task.FromResult
-        else
-          downloadIdsAsync' userId (offset + 50)
+        let! nextTracksIds =
+          if tracks.Next = null then
+            [] |> Task.FromResult
+          else
+            downloadIdsAsync' userId (offset + 50) ()
 
-      let currentTracksIds =
-        tracks.Items
-        |> List.ofSeq
-        |> List.map (fun x -> x.Track.Id)
-        |> List.map RawTrackId.create
+        let currentTracksIds =
+          tracks.Items
+          |> List.ofSeq
+          |> List.map (fun x -> x.Track.Id)
+          |> List.map RawTrackId.create
 
-      return List.append nextTracksIds currentTracksIds
-    }
+        return List.append nextTracksIds currentTracksIds
+      }
 
   member _.ListIdsAsync userId refreshCache =
     task {
--- src/Generator.Worker/Services/PlaylistService.fs
+++ src/Generator.Worker/Services/PlaylistService.fs
@@ -7,27 +7,27 @@ open Shared.Services
 
 type PlaylistService(_spotifyClientProvider: SpotifyClientProvider, _idsService: TracksIdsService) =
   let rec downloadTracksIdsAsync' (userId: int64) playlistId (offset: int) =
-    task {
-      let client =
-        _spotifyClientProvider.Get userId
+    fun () ->
+      task {
+        let client = _spotifyClientProvider.Get userId
 
-      let! tracks = client.Playlists.GetItems(playlistId, PlaylistGetItemsRequest(Offset = offset))
+        let! tracks = client.Playlists.GetItems(playlistId, PlaylistGetItemsRequest(Offset = offset))
 
-      let! nextTracksIds =
-        if tracks.Next = null then
-          [] |> Task.FromResult
-        else
-          downloadTracksIdsAsync' userId playlistId (offset + 100)
+        let! nextTracksIds =
+          if tracks.Next = null then
+            [] |> Task.FromResult
+          else
+            downloadTracksIdsAsync' userId playlistId (offset + 100) ()
 
-      let currentTracksIds =
-        tracks.Items
-        |> List.ofSeq
-        |> List.map (fun x -> x.Track :?> FullTrack)
-        |> List.map (fun x -> x.Id)
-        |> List.map RawTrackId.create
+        let currentTracksIds =
+          tracks.Items
+          |> List.ofSeq
+          |> List.map (fun x -> x.Track :?> FullTrack)
+          |> List.map (fun x -> x.Id)
+          |> List.map RawTrackId.create
 
-      return List.append nextTracksIds currentTracksIds
-    }
+        return List.append nextTracksIds currentTracksIds
+      }
 
   let downloadTracksIdsAsync userId playlistId =
     downloadTracksIdsAsync' userId playlistId 0
