8b9dc0225c1d806b1ea3367d1849bf0464a7c786
Oleg <otsomkalov@proton.me>
2023-03-12 14:28:21 +0200

    Fix not found playlist handling

--- src/Generator.Worker/Services/PlaylistService.fs
+++ src/Generator.Worker/Services/PlaylistService.fs
@@ -2,32 +2,37 @@
 
 open System.Threading.Tasks
 open Generator.Worker.Domain
+open Microsoft.Extensions.Logging
 open SpotifyAPI.Web
 open Shared.Services
 
-type PlaylistService(_spotifyClientProvider: SpotifyClientProvider, _idsService: TracksIdsService) =
+type PlaylistService(_spotifyClientProvider: SpotifyClientProvider, _idsService: TracksIdsService, _logger: ILogger<PlaylistService>) =
   let rec downloadTracksIdsAsync' (userId: int64) playlistId (offset: int) =
     fun () ->
       task {
         let client = _spotifyClientProvider.Get userId
 
-        let! tracks = client.Playlists.GetItems(playlistId, PlaylistGetItemsRequest(Offset = offset))
+        try
+          let! tracks = client.Playlists.GetItems(playlistId, PlaylistGetItemsRequest(Offset = offset))
 
-        let! nextTracksIds =
-          if tracks.Next = null then
-            [] |> Task.FromResult
-          else
-            downloadTracksIdsAsync' userId playlistId (offset + 100) ()
+          let! nextTracksIds =
+            if tracks.Next = null then
+              [] |> Task.FromResult
+            else
+              downloadTracksIdsAsync' userId playlistId (offset + 100) ()
 
-        let currentTracksIds =
-          tracks.Items
-          |> List.ofSeq
-          |> List.filter (fun t -> isNull t.Track |> not)
-          |> List.map (fun x -> x.Track :?> FullTrack)
-          |> List.map (fun x -> x.Id)
-          |> List.map RawTrackId.create
+          let currentTracksIds =
+            tracks.Items
+            |> List.ofSeq
+            |> List.filter (fun t -> isNull t.Track |> not)
+            |> List.map (fun x -> x.Track :?> FullTrack)
+            |> List.map (fun x -> x.Id)
+            |> List.map RawTrackId.create
 
-        return List.append nextTracksIds currentTracksIds
+          return List.append nextTracksIds currentTracksIds
+        with _ ->
+          _logger.LogInformation("Playlist with id {PlaylistId} not found in Spotify", playlistId)
+          return []
       }
 
   let downloadTracksIdsAsync userId playlistId =
