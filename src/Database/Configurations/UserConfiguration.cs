baeb22b789960adb0325dde7ada9d756372a0125
Oleh <otsomkalov@proton.me>
2023-06-09 21:29:36 +0300

    Move database entities configurations to separate files

---
+++ src/Database/Configurations/UserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Database.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder
+            .OwnsOne(u => u.Settings, settings =>
+            {
+                settings.Property(s => s.PlaylistSize)
+                    .HasDefaultValue(20);
+            });
+    }
+}
