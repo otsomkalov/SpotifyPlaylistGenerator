900062e996b59e8c6f39fc157ae141d78a0244be
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-06-09 19:03:47 +0300

    Extract enabling

--- src/Telegram.Tests/PresetSettings.fs
+++ src/Telegram.Tests/PresetSettings.fs
@@ -10,10 +10,11 @@ open Xunit
 open Telegram.Workflows
 
 [<Fact>]
-let ``disableUniqueArtists should update preset and send show updated`` () =
+let ``enableUniqueArtists should update preset and send show updated`` () =
   let disableUniqueArtists =
     fun presetId ->
       presetId |> should equal Preset.mockId
+
       Task.FromResult()
 
   let answerCallbackQuery = fun text -> Task.FromResult()
@@ -21,6 +22,28 @@ let ``disableUniqueArtists should update preset and send show updated`` () =
   let showPresetInfo =
     fun presetId ->
       presetId |> should equal Preset.mockId
+
+      Task.FromResult()
+
+  let sut =
+    PresetSettings.enableUniqueArtists disableUniqueArtists answerCallbackQuery showPresetInfo
+
+  sut Preset.mockId
+
+[<Fact>]
+let ``disableUniqueArtists should update preset and send show updated`` () =
+  let disableUniqueArtists =
+    fun presetId ->
+      presetId |> should equal Preset.mockId
+
+      Task.FromResult()
+
+  let answerCallbackQuery = fun text -> Task.FromResult()
+
+  let showPresetInfo =
+    fun presetId ->
+      presetId |> should equal Preset.mockId
+
       Task.FromResult()
 
   let sut =
