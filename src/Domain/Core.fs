4fd651ecf9c3e90462a9835a62f2ad15cf476906
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-11-03 15:52:52 +0200

    Fix build

--- src/Domain/Core.fs
+++ src/Domain/Core.fs
@@ -167,8 +167,6 @@ module User =
 
   type CreatePreset = UserId -> string -> Task<Preset>
 
-  type CreatePreset = UserId -> string -> Task<Preset>
-
   let create userId =
     { Id = userId
       CurrentPresetId = None
