4399d1434410c28479dce4c02fd57377067dd1b2
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-11-16 11:55:38 +0200

    Fix after merge

--- src/Infrastructure.Telegram/Services.fs
+++ src/Infrastructure.Telegram/Services.fs
@@ -26,6 +26,7 @@ open otsom.fs.Telegram.Bot.Auth.Spotify.Settings
 open otsom.fs.Telegram.Bot.Core
 open otsom.fs.Core
 open Infrastructure.Repos
+open Domain.Repos
 
 type AuthState =
   | Authorized
