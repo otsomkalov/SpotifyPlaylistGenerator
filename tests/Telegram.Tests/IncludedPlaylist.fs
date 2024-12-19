ae595748f2f4fd3495d00dab48802d9b4516a971
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-12-19 16:45:40 +0200

    Use BotMessageCtx EditMessageButtons

--- tests/Telegram.Tests/IncludedPlaylist.fs
+++ tests/Telegram.Tests/IncludedPlaylist.fs
@@ -3,12 +3,14 @@
 open System.Threading.Tasks
 open Domain.Core
 open Domain.Tests
+open Moq
 open Telegram.Bot.Types.ReplyMarkups
 open Telegram.Core
 open FsUnit.Xunit
 open Xunit
 open Domain.Workflows
 open Telegram.Workflows
+open otsom.fs.Bot
 
 let getPreset =
   fun presetId ->
@@ -18,23 +20,28 @@ let getPreset =
 
 [<Fact>]
 let ``list should send included playlists`` () =
-  let editMessageButtons =
-    fun text (replyMarkup: InlineKeyboardMarkup) ->
-      replyMarkup.InlineKeyboard |> Seq.length |> should equal 2
+  let botMessageCtx =
+    { new IEditMessageButtons with
+        member this.EditMessageButtons =
+          fun text buttons ->
+            buttons |> Seq.length |> should equal 2
 
-      Task.FromResult()
+            Task.FromResult() }
 
-  let sut = IncludedPlaylist.list getPreset editMessageButtons
+  let sut = IncludedPlaylist.list getPreset botMessageCtx
 
   sut Mocks.preset.Id (Page 0)
 
 [<Fact>]
 let ``show should send included playlist details`` () =
-  let editMessageButtons =
-    fun text (replyMarkup: InlineKeyboardMarkup) ->
-      replyMarkup.InlineKeyboard |> Seq.length |> should equal 3
 
-      Task.FromResult()
+  let botMessageCtx =
+    { new IEditMessageButtons with
+        member this.EditMessageButtons =
+          fun text buttons ->
+            buttons |> Seq.length |> should equal 3
+
+            Task.FromResult() }
 
   let countPlaylistTracks =
     fun playlistId ->
@@ -43,7 +50,7 @@ let ``show should send included playlist details`` () =
 
       0L |> Task.FromResult
 
-  let sut = IncludedPlaylist.show editMessageButtons getPreset countPlaylistTracks
+  let sut = IncludedPlaylist.show botMessageCtx getPreset countPlaylistTracks
 
   sut Mocks.presetId Mocks.includedPlaylist.Id
 
@@ -55,15 +62,17 @@ let ``remove should delete playlist and show included playlists`` () =
       playlistId |> should equal Mocks.includedPlaylist.Id
       Task.FromResult()
 
-  let editMessageButtons =
-    fun text (replyMarkup: InlineKeyboardMarkup) ->
-      replyMarkup.InlineKeyboard |> Seq.length |> should equal 2
+  let botMessageCtx =
+    { new IEditMessageButtons with
+        member this.EditMessageButtons =
+          fun text buttons ->
+            buttons |> Seq.length |> should equal 2
 
-      Task.FromResult()
+            Task.FromResult() }
 
   let showNotification = fun _ -> Task.FromResult()
 
   let sut =
-    IncludedPlaylist.remove getPreset editMessageButtons removePlaylist showNotification
+    IncludedPlaylist.remove getPreset botMessageCtx removePlaylist showNotification
 
   sut Mocks.presetId Mocks.includedPlaylist.Id
